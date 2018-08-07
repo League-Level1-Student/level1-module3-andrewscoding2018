466f02b9e4cc6a1beda2a907e3c232b5262e74d1
Samuel Sharp <league@WTS7.attlocal.net>
2018-08-07 14:54:30 -0700

    hgjhgjhghgjgjhg

---
+++ src/NastySuprise.java
@@ -0,0 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class NastySuprise {
+
+	public void createButton()
+{
+JFrame frame = new JFrame();
+JPanel panel  = new JPanel();
+JButton button  = new JButton();
+JButton button1 = new JButton();
+frame.add(panel);
+panel.add(button);
+panel.add(button1);
+
+
+}
+}
